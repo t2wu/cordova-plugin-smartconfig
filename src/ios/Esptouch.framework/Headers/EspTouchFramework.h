7d316f1c45f339a9b838c1de6fd43941334b099e
Timothy Wu <g39328006@ym.edu.tw>
2018-03-06 22:56:33 +0800

    Update Esptouch framework to support iOS platform since 9.0

---
+++ src/ios/Esptouch.framework/Headers/EspTouchFramework.h
@@ -0,0 +1,19 @@
+//
+//  EspTouchFramework.h
+//  EspTouchFramework
+//
+//  Created by Timothy Wu on 2018/1/29.
+//  Copyright © 2018 白 桦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EspTouchFramework.
+FOUNDATION_EXPORT double EspTouchFrameworkVersionNumber;
+
+//! Project version string for EspTouchFramework.
+FOUNDATION_EXPORT const unsigned char EspTouchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EspTouchFramework/PublicHeader.h>
+
+
